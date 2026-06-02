ef0ebc9ab21ca695430247e8f10dea2720168d08
raysilent7 <53231079+raysilent7@users.noreply.github.com>
2026-06-02 18:58:28 -0300

    criando escuridao
    - criando zona escura e ponto de luz do mapa que segue o jogador.
    - camera movida do jogador do mundo real para o centro da fase, assim ela sempre fica focada no centro e nao mais no jogador do mundo real, que ocasionava que o jogador do mundo invertido as vezes fosse cortado da tela.

--- scripts/levels/level0.gd
+++ scripts/levels/level0.gd
@@ -1,4 +1,18 @@
 extends Node
 
+@onready var camera: Camera2D = $camera
+@onready var player: Node2D = $Player
+@onready var light: PointLight2D = $light
+@onready var darkness: CanvasModulate = $darkness
+
 func _ready() -> void:
 	GameState.hasKey = false
+	GameState.isDark = darkness.visible
+	if GameState.isDark:
+		light.enabled = true
+	else:
+		light.enabled = false
+
+func _process(_delta: float) -> void:
+	camera.global_position.x = player.p1.global_position.x
+	light.global_position = camera.global_position
