2d103c59d3512c3f8547991ade2c9bb721b7b4be
raysilent7 <53231079+raysilent7@users.noreply.github.com>
2026-06-04 15:33:11 -0300

    adicionando som do vazamento de vapor
    adicionando som do vazamento de vapor

--- scripts/scenario/steel.gd
+++ scripts/scenario/steel.gd
@@ -2,7 +2,6 @@ extends Node2D
 
 @onready var sprite: Sprite2D = $Sprite2D
 @onready var particles: GPUParticles2D = $GPUParticles2D
-@onready var audio_player: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 @export var min_speed: float = 1.0     
 @export var max_speed: float = 2.5     
@@ -35,8 +34,7 @@ func choose_next_action() -> void:
 			target_speed = 0.0
 			if particles:
 				particles.emitting = false
-			if audio_player:
-				audio_player.stop()
+			AudioManager.stopSteamLeak()
 			
 		1, 2:
 			if random_choice == 1:
@@ -47,7 +45,6 @@ func choose_next_action() -> void:
 			if particles:
 				particles.emitting = true
 
-			if audio_player and not audio_player.playing:
-				audio_player.play()
+			AudioManager.playSteamLeak()
 			
 	action_timer = randf_range(min_duration, max_duration)
